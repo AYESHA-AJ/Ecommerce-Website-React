97ae08f36a63bfb20f6f6c7afb80fdc2b6ad27c1
AYESHA-AJ <ayeshaaj25@gmail.com>
2024-05-24 20:07:37 +0500

    card creation

--- EcommerceWebsite/src/Home/HomeCategory.jsx
+++ EcommerceWebsite/src/Home/HomeCategory.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 const subTitle = "Choose Any Products";
 const title = "Buy Everything with Us";
 const btnText = "Get Started Now";
@@ -45,8 +45,41 @@ title: 'Home Decor',
 
 const HomeCategory = () => {
   return (
-    <div>
-      home category
+    <div className='category-section style-4 padding-tb'>
+      <div className='container'>
+        {/* section header */}
+        <div className='section-header text-center'>
+          <span className='subtitle'>{subTitle}</span>
+          <h2 className='title'>{ title}</h2>
+        </div>
+        {/* section cards */}
+        <div className='section-wrapper'>
+          <div className='row g-4 justify-content-center row-cols-md-3 row-cols-sm-2 row-cols-1'>
+            {
+              categoryList.map((val, i) => (<div key={i} className='col'>
+                <Link to="/shop" className='category-item'>
+                  <div className='category-inner'>
+                    {/* image thumbnail */}
+                    <div className='category-thumb'>
+                      <img src={val.imgUrl} alt=""/>
+                    </div>
+                    {/* content */}
+                    <div className='category-content'>
+                      <div className='cate-icon'>
+                    <i className={val.iconName}></i>
+                      </div>
+                      <Link to="/shop"><h6>{ val.title}</h6></Link>
+                    </div>
+                  </div>
+                </Link>
+              </div>))
+            }
+          </div>
+          <div className='text-center mt-5'>
+            <Link to="/shop" className='lab-btn'><span>{ btnText}</span></Link>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
